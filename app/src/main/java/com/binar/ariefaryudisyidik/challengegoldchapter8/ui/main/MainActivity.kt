3b44b5cca53ee988a00eb9968cf523572ec3d6f2
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-06-12 09:11:10 +0700

    Add Splash Screen

--- app/src/main/java/com/binar/ariefaryudisyidik/challengegoldchapter8/ui/main/MainActivity.kt
+++ app/src/main/java/com/binar/ariefaryudisyidik/challengegoldchapter8/ui/main/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.binar.ariefaryudisyidik.challengegoldchapter8
+package com.binar.ariefaryudisyidik.challengegoldchapter8.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,11 +11,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.binar.ariefaryudisyidik.challengegoldchapter8.ui.theme.ChallengeGoldChapter8Theme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { viewModel.isLoading.value }
+        }
         super.onCreate(savedInstanceState)
+
         setContent {
             ChallengeGoldChapter8Theme {
                 Surface(
---
+++ app/src/main/java/com/binar/ariefaryudisyidik/challengegoldchapter8/ui/main/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.binar.ariefaryudisyidik.challengegoldchapter8.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(1)
+            _isLoading.value = false
+        }
+    }
+}
